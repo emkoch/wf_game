ffb9409cc9017366af37a1b76cc9af935b309fb2
Evan M Koch <emkoch@uchicago.edu>
2017-10-10 14:14:24 -0500

    better random colors

--- stupidcat.py
+++ stupidcat.py
@@ -2,6 +2,8 @@ import pygame, sys
 from pygame.locals import *
 from pygame import gfxdraw
 from catboard import *
+from matplotlib import cm
+import random
 
 FPS = 30
 WINDOWWIDTH = 1000
@@ -29,15 +31,15 @@ def main():
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYUP and event.key == K_SPACE and not CATBOARD.is_poly():
+                new_cat_color = random_tab20()
                 new_cat_color = (random.randint(0, 255),
                                  random.randint(0, 255),
                                  random.randint(0, 255))
                 CATBOARD.mutate_cats(0.5, new_cat_color)
                 CATBOARD.COLOR_FIT[new_cat_color] = 1.
             elif event.type == KEYUP and event.key == K_RSHIFT and not CATBOARD.is_poly():
-                new_cat_color = (random.randint(0, 255),
-                                 random.randint(0, 255),
-                                 random.randint(0, 255))
+                new_cat_color = random_tab20()
+                print(new_cat_color)
                 CATBOARD.mutate_cats(0.1, new_cat_color)
                 CATBOARD.COLOR_FIT[new_cat_color] = 1.5
 
@@ -46,6 +48,10 @@ def main():
         CATBOARD.add_new_gen_sel()
         if not CATBOARD.is_poly():
             CATBOARD.COLOR_FIT = {CATBOARD.CAT_STATES[-1][0][2]:1.}
+
+def random_tab20():
+    viridis_val = cm.get_cmap('tab20')(random.randint(0, 19))
+    return (viridis_val[0]*255., viridis_val[1]*255., viridis_val[2]*255.)
 
 if __name__ == "__main__":
     main()
